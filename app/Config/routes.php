4bd6a94c8ae75e4d84bc64288b5f0b873ed57793
xuantai <study@ip-172-31-4-113.ap-northeast-1.compute.internal>
2015-09-28 04:34:22 +0000

    vol/01 complete

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -35,6 +35,11 @@
  * how to customize the loading of plugin routes.
  */
 	CakePlugin::routes();
+/*
+* API
+*/
+Router::mapResources(array ('todo_lists',));
+Router::parseExtensions('json');
 
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
